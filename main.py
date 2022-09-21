7dc8f00f0594ab5d4e32e18f81b553dd2826746b
OlegCeceli10 <833980@mystma.org>
2022-09-21 09:23:23 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
 #WRITE YOUR CODE IN THIS FILE
+def greaterThan(x,y):
+    if x<y:
+        return False
+    else:
+        return true
+    
+def lessThan(x,y):
+    if x<y:
+        return True
+    else:
+        return False
+
+def equalTo(x,y):
+    if x==y:
+        return True
+    else:
+        return False
+
+def greaterOrEqual(x,y):
+    if x>=y:
+        return True
+    else:
+        return False
+
+def lessOrEqual(x,y):
+    if x<=y:
+        return True
+    else: 
+        return False
+
+print (lessOrEqual(10, 200))
